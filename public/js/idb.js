f1f03781afcd38698d7291579dabb2fdce894eb9
Mark S <marksherm04@gmail.com>
2021-08-19 14:21:26 -0400

    updated idb.js and creating service workers

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,7 +4,7 @@ let db;
 const request = indexedDB.open('pwa-budget-tracker', 1);
 
 // this event will emit database changes
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
 	// save reference to the database
 	const db = event.target.result;
 	// create an object store called 'new_input', set it to have an auto incrementing primary key
@@ -12,17 +12,17 @@ request.onupgradeneeded = function(event) {
 };
 
 // upon a successful request
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
 	// when db is successfully created with it's object store (from onupgradedneeded event) or an established connection happens, save reference to db in global variable
 	db = event.target.result;
 
 	// check if app is online, if yes run updateBudget() function to send all local db data to api
 	if (navigator.onLine) {
-		// updateBudget();
+		updateBudget();
 	}
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
 	// log error here
 	console.log(event.target.errorCode);
 };
@@ -39,7 +39,7 @@ function saveRecord(transaction) {
 	inputObjectStore.add(transaction)
 }
 
-function uploadTransaction() {
+function updateBudget() {
 	// open a tarnsaction on your db
 	const transaction = db.transaction(['new_input'], 'readwrite');
 
@@ -49,5 +49,41 @@ function uploadTransaction() {
 	// get all records from store and set to variable
 	const getAll = inputObjectStore.getAll();
 
-	// more here soon
+	// upon successful .getAll() execution, run this function
+	getAll.onsuccess - function () {
+		// if there was data in indexedDb's store, send it to the api server
+		if (getAll.result.length > 0) {
+			// TODO: NEED TO CHECK CODE HERE
+			fetch('/routes/api.js', {
+				method: 'POST',
+				body: JSON.stringify(getAll.result),
+				headers: {
+					Accept: 'application/json, text/plain, */*',
+					'Content-Type': 'application/json'
+				}
+			})
+				.then(response => response.json())
+				.then(serverResponse => {
+					if (serverResponse.message) {
+						throw new Error(serverResponse);
+					}
+					// open one more transaction
+					const transaction = db.transaction(['new_input'], 'readwrite');
+					// access the new_input object store
+					const inputObjectStore = transaction.objectStore('new_input');
+					// clear all items in your store
+					pizzaObjectStore.clear();
+
+					alert('All saved transactions has been submitted!');
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		}
+	}
+
+	// listen for app coming back online
+	window.addEventListener('online', updateBudget);
+
 }
+
